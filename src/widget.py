30b9706787c6ea616b9a30022ff96935081d71fd
Anton Masloynok <antoxa.toxa.83@mail.ru>
2024-12-26 23:05:22 +0300

    Создали две функции: mask_account_card и get_date!

--- src/widget.py
+++ src/widget.py
@@ -19,7 +19,7 @@ def get_date(line: str) -> str:
 
     return f'{line[8:10]}.{line[5:7]}.{line[0:4]}'
 
-
+print(mask_account_card('Счет 73654108430135874305'))
 print(mask_account_card('Visa Platinum 7000792289606361'))
 print(get_date("2024-03-11T02:26:18.671407"))
 
