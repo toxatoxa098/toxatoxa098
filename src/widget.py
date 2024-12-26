a896e26f0b287b285616a93c6080fc026b231e20
Anton Masloynok <antoxa.toxa.83@mail.ru>
2024-12-27 00:02:56 +0300

    Based on the functions from the masks module, we created a new function for processing data maps

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,7 @@
-def mask_account_card(account_card: [str]) -> str:
+from typing import Union
+
+
+def mask_account_card(account_card: Union[str]) -> Union[str]:
     """Функция, которая умеет обрабатывать информацию как о картах, так и о счетах."""
 
     parts = account_card.split()
@@ -13,14 +16,13 @@ def mask_account_card(account_card: [str]) -> str:
     return f"{account_type} {masked_number}"
 
 
-def get_date(line: str) -> str:
+def get_date(line: Union[str]) -> Union[str]:
     """Функция,которая принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
  и возвращает строку с датой в формате "ДД.ММ.ГГГГ"("11.03.2024")."""
 
     return f'{line[8:10]}.{line[5:7]}.{line[0:4]}'
 
+
 print(mask_account_card('Счет 73654108430135874305'))
 print(mask_account_card('Visa Platinum 7000792289606361'))
 print(get_date("2024-03-11T02:26:18.671407"))
-
-
